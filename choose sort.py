ea78b3a84fff4a5ceccb9a19510abd74b33b9513
sheep <2562789632@qq.com>
2021-12-16 23:12:46 +0800

    添加学习目标
    添加学习目标:程序设计
    
    添加学习目标:程序设计
    
    添加学习目标:程序设计

--- choose sort.py
+++ choose sort.py
@@ -1,9 +1,10 @@
 def choose_sort(numlist):
     reslist = []
-    while(len(numlist)>0):
+    while len(numlist) > 0:
         reslist.append(find_max(numlist))
         numlist.remove(find_max(numlist))
     return reslist
+
 
 def find_max(numlist):
     test = 0
@@ -13,6 +14,7 @@ def find_max(numlist):
     maxnum = test
     return maxnum
 
-numlist = [44,5,1,7,15,4,66,134,65,4,6,3,5,90,78,45]
+
+numlist = [44, 5, 1, 7, 15, 4, 66, 134, 165, 4, 6, 3, 5, 90, 78, 45]
 res = choose_sort(numlist)
 print(res)
--- qsort.py
+++ qsort.py
@@ -2,15 +2,17 @@ def qsort(numlist):
     if len(numlist) < 2:
         return numlist
     else:
-        front=[]
-        back =[]
+        front = []
+        back = []
         for i in numlist:
-            if i>numlist[0]:
+            if i > numlist[0]:
                 front.append(i)
-            if i<numlist[0]:
+            if i < numlist[0]:
                 back.append(i)
-        res = qsort(front) +[numlist[0]] +qsort(back)
+        res = qsort(front) + [numlist[0]] + qsort(back)
         return res
-numlist = [44,5,1,7,15,4,66,134,65,4,6,3,5,90,78,45]
+
+
+numlist = [44, 5, 1, 7, 15, 4, 66, 134, 65, 4, 6, 3, 5, 90, 78, 45]
 res = qsort(numlist)
 print(res)
