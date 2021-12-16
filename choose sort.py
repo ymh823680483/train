70f82d7a44db954ca1af8137a78c5d3105c286d3
sheep <2562789632@qq.com>
2021-12-16 23:09:04 +0800

    init commit

---
+++ choose sort.py
@@ -0,0 +1,18 @@
+def choose_sort(numlist):
+    reslist = []
+    while(len(numlist)>0):
+        reslist.append(find_max(numlist))
+        numlist.remove(find_max(numlist))
+    return reslist
+
+def find_max(numlist):
+    test = 0
+    for i in numlist:
+        if test < i:
+            test = i
+    maxnum = test
+    return maxnum
+
+numlist = [44,5,1,7,15,4,66,134,65,4,6,3,5,90,78,45]
+res = choose_sort(numlist)
+print(res)
